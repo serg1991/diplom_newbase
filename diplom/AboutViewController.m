dc3c57e338debbe231600e8031516a9243e14e6a
serg1991 <Sergey@MacBook-Pro-Sergey.local>
2014-03-25 23:37:19 +0400

    revert to back button title

--- diplom/AboutViewController.m
+++ diplom/AboutViewController.m
@@ -27,16 +27,31 @@
     [super viewDidLoad];
 
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"UINavigationBarBackIndicatorDefault"]];
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, imageView.frame.size.width + 30, imageView.frame.size.height)];
+    UILabel *labelback = [[UILabel alloc] init];
+    [labelback setText:@"Меню"];
+    [labelback sizeToFit];
+    
+    int space = 6;
+    labelback.frame = CGRectMake(imageView.frame.origin.x + imageView.frame.size.width + space, labelback.frame.origin.y, labelback.frame.size.width, labelback.frame.size.height);
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, labelback.frame.size.width + imageView.frame.size.width + space, imageView.frame.size.height)];
     
     view.bounds = CGRectMake(view.bounds.origin.x + 8, view.bounds.origin.y - 1, view.bounds.size.width, view.bounds.size.height);
     [view addSubview:imageView];
+    [view addSubview:labelback];
     
     UIButton *button = [[UIButton alloc] initWithFrame:view.frame];
     [button addTarget:self action:@selector(confirmCancel) forControlEvents:UIControlEventTouchUpInside];
     [view addSubview:button];
     
+    [UIView animateWithDuration:0.33 delay:0 options:UIViewAnimationOptionCurveLinear animations:^{
+        labelback.alpha = 0.0;
+        CGRect orig = labelback.frame;
+        labelback.frame = CGRectMake(labelback.frame.origin.x + 25, labelback.frame.origin.y, labelback.frame.size.width, labelback.frame.size.height);
+        labelback.alpha = 1.0;
+        labelback.frame = orig;
+    } completion:nil];
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithCustomView:view];
+    
     self.navigationItem.leftBarButtonItem = backButton;
 }
 
